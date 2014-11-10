3b428166a2aee2159fb8bf68b713e9bc854675b5
shaodongying <shaodongying@gmsdtech.com>
2014-11-10 14:40:13 +0800

    添加计划状态字段

--- src/com/gmsd/model/Plan.java
+++ src/com/gmsd/model/Plan.java
@@ -8,105 +8,109 @@ package com.gmsd.model;
  * 理财计划。
  */
 public class Plan {
-  /**
-   * 计划ID
-   */
-  public Long id;
+    /**
+     * 计划ID
+     */
+    public Long id;
 
-  /**
-   * 计划名称
-   */
-  public String name;
+    /**
+     * 计划名称
+     */
+    public String name;
 
-  /**
-   * 详细描述
-   */
-  public String description;
+    /**
+     * 详细描述
+     */
+    public String description;
 
-  /**
-   * 产品到期数字
-   */
-  public Integer maturityDuration;
+    /**
+     * 产品到期数字
+     */
+    public Integer maturityDuration;
 
-  /**
-   * 产品到期单位
-   */
-  public DurationUnit maturityUnit;
+    /**
+     * 产品到期单位
+     */
+    public DurationUnit maturityUnit;
 
-  /**
-   * 基础锁定期数字
-   */
-  public Integer lockDuration;
+    /**
+     * 基础锁定期数字
+     */
+    public Integer lockDuration;
 
-  /**
-   * 基础锁定期单位
-   */
-  public DurationUnit lockUnit;
+    /**
+     * 基础锁定期单位
+     */
+    public DurationUnit lockUnit;
 
-  /**
-   * 预计最小年化收益
-   */
-  public String minInterest;
+    /**
+     * 预计最小年化收益
+     */
+    public String minInterest;
 
-  /**
-   * 预计最大年化收益
-   */
-  public String maxInterest;
+    /**
+     * 预计最大年化收益
+     */
+    public String maxInterest;
 
 
-  /**
-   * 加入额度的下限 （0表示没有限制）
-   */
-  public String minAmount;
+    /**
+     * 加入额度的下限 （0表示没有限制）
+     */
+    public String minAmount;
 
-  /**
-   * 加入额度的上限 （0表示没有限制）
-   */
-  public String maxAmount;
+    /**
+     * 加入额度的上限 （0表示没有限制）
+     */
+    public String maxAmount;
 
-  /**
-   * 加入额度的基数 （0表示没有限制）
-   */
-  public String baseAmount;
+    /**
+     * 加入额度的基数 （0表示没有限制）
+     */
+    public String baseAmount;
 
-  /**
-   * 计划总额度
-   */
-  public String totalAmount;
+    /**
+     * 计划总额度
+     */
+    public String totalAmount;
 
-  /**
-   * 计划剩余可卖额度
-   */
-  public String remainingAmount;
+    /**
+     * 计划剩余可卖额度
+     */
+    public String remainingAmount;
 
-  /**
-   * 已确认付款额度
-   */
-  public String paidAmount;
+    /**
+     * 已确认付款额度
+     */
+    public String paidAmount;
 
-  /**
-   * 计划类别
-   */
-  public PlanType type;
+    /**
+     * 计划类别
+     */
+    public PlanType type;
 
-  /**
-   * 销售类别
-   */
-  public SaleType saleType;
+    /**
+     * 销售类别
+     */
+    public SaleType saleType;
 
-  /**
-   * 还款方式
-   */
-  public RefundMethod refundType;
+    /**
+     * 还款方式
+     */
+    public RefundMethod refundType;
 
-  /**
-   * 是否可主动赎回
-   */
-  public Integer redeem;
+    /**
+     * 是否可主动赎回
+     */
+    public Integer redeem;
 
-  /**
-   * 售卖截止日期
-   */
-  public String endDate;
+    /**
+     * 售卖截止日期
+     */
+    public String endDate;
 
+    /**
+     * 计划状态
+     */
+    public PlanStatus status;
 }
